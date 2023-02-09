181af8e75e52ff5427274d986f3b9b2d198b6e72
DemonVenom <xxspamxxemailxx@gmail.com>
2023-02-09 17:39:08 -0500

    Solved ex02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,20 @@
-const repeatString = function() {
+const repeatString = function(phrase, num) {
+
+    let result = "";
+
+    if (num >= 0) {
+
+        for (let i = 0; i < num; i++) {
+
+            result += phrase;
+        }
+
+    }
+    else {
+        result += "ERROR";
+    }
+
+    return result;
 
 };
 
