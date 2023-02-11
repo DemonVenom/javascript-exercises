59d6181df6861eb7d26bac1046d96511bb99b78f
DemonVenom <xxspamxxemailxx@gmail.com>
2023-02-11 15:43:10 -0500

    Solved ex04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(input, ...args) {
+
+    return input.filter(element => !args.includes(element));
+
+    // var removes = new Set(args);
+    //
+    // return input.filter(function(args) {
+    //     return !removes.has(args)
+    // });
 
 };
 
