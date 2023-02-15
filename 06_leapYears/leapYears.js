311abbb94f15d43b7677497a667258bfb79da3e0
DemonVenom <xxspamxxemailxx@gmail.com>
2023-02-14 22:38:08 -0500

    Solved ex06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+const leapYears = function(input) {
+
+    if (input % 4 === 0 && (input % 100 !== 0 || input % 400 === 0)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 
 };
 
