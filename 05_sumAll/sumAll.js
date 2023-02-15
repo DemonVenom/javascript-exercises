016097b9d2e730f62951acec91d66a87ab1338fc
DemonVenom <xxspamxxemailxx@gmail.com>
2023-02-14 22:10:55 -0500

    Solved ex05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,41 @@
-const sumAll = function() {
+const sumAll = function(input, asdf) {
+
+    let result = 0
+
+    if (input < 0 || asdf < 0) {
+
+        return "ERROR";
+    }
+    else if (input < 0 || asdf < 0) {
+
+        return "ERROR";
+    }
+    else if (!Number.isInteger(input) || !Number.isInteger(asdf)) {
+
+        return "ERROR";
+    }
+    else if (input > asdf) {
+
+        for (let i = asdf; i <= input; i++) {
+
+            result += i;
+        }
+
+        return result;
+    }
+    else if (input < asdf) {
+
+        for (let i = input; i <= asdf; i++) {
+
+            result += i;
+        }
+
+        return result;
+    }
+    else {
+
+        return -1;
+    }
 
 };
 
